e64c34102cac870929190bf86c6bb9ac59eed609
Conner Martin <connerjm2@gmail.com>
2021-04-16 21:11:45 -0700

    Added the models.

---
+++ routes/api.js
@@ -0,0 +1,11 @@
+//get workouts.
+//get api/workouts.
+
+//add exercise
+//put api/workouts:id
+
+//create workout
+//post api/workouts
+
+//get workouts in range
+//api/workouts/range
---
+++ server.js
@@ -0,0 +1,30 @@
+//Imports.
+const express = require("express");
+const mongoose = require("mongoose");
+
+//Set port.
+const PORT = process.env.PORT || 8008;
+
+//Create server.
+const app = express();
+
+//Set middleware.
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+//Set mongoose connection.
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout",
+{
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+//Routes.
+//app.use(require("./routes/api.js"));
+
+//Start server.
+app.listen(PORT, () =>
+{
+    console.log(`App running on port ${PORT}!`);
+});
